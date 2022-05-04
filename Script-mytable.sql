8f4f1e0882ef751bd96ba0eb81f35a1b77365c20
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2022-05-04 19:22:42 -0300

    Adicionando os meus arquivos

---
+++ Script-mytable.sql
@@ -0,0 +1,5 @@
+select * from mytable where idade > 20;
+select * from mytable where anosdeexperiencianaarea > 4;
+
+select count(idade) from mytable where idade > 20; 
+select count(anosdeexperiencianaarea) from mytable where anosdeexperiencianaarea > 4;
