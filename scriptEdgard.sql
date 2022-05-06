44be4d37a7214609c82db1992160a58aae2b0f42
ehctds <100098820+ehctds@users.noreply.github.com>
2022-05-05 19:47:49 -0700

    Add files via upload

---
+++ scriptEdgard.sql
@@ -0,0 +1,15 @@
+SELECT nome, idade
+from mytable 
+WHERE idade > 20
+
+SELECT nome, anosdeexperiencianaarea 
+from mytable 
+WHERE anosdeexperiencianaarea  > 4
+
+SELECT count(idade)
+FROM mytable
+where idade < 20
+
+select count(anosdeexperiencianaarea)
+from mytable
+where anosdeexperiencianaarea < 4
