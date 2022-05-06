db3be01845ba3ec901bceac65e14e586d6975fb1
carinebs <krinecbs@gmail.com>
2022-05-06 12:49:52 -0300

    arquivo sql

---
+++ consulta_dados.sql
@@ -0,0 +1,12 @@
+
+select nome, idade  from mytable 
+where idade  > 20
+
+select nome, anosdeexperiencianaarea  from mytable 
+where anosdeexperiencianaarea  > 4
+
+select count(*) from mytable 
+where idade < 20
+
+select count(*) from mytable 
+where anosdeexperiencianaarea  < 4
